7aa16069f0511d6d5313eff24dc0d0047c38dc44
Brent <heybrentcrosby@gmail.com>
2026-09-07 13:53:30 -0700

    feat: implement deterministic PDF metadata vertical slice

---
+++ src/main/java/com/brentcrosby/pdfaccessibilityassistant/PdfAccessibilityAssistantApplication.java
@@ -0,0 +1,12 @@
+package com.brentcrosby.pdfaccessibilityassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfAccessibilityAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfAccessibilityAssistantApplication.class, args);
+    }
+}
